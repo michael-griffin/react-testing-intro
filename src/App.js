982a94d766a0c2dbacb2d0a85cf73ca8f75aa8fd
Daniel Zych <danjzych@gmail.com>
2023-09-26 18:43:24 -0500

    Completed coinflipper

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import Carousel from "./Carousel";
 import photos from "./photos.js";
+import CoinFlipper from "./CoinFlipper";
 
 /** App component, renders Carousel
- * 
+ *
  * Props: none
  * State: none
- * 
+ *
  * App --> Carousel
  */
 
 function App() {
-
   // In real life, this data would probably come from an AJAX call.
   // For our purposes, we're just importing from another file.
   const carouselPhotos = photos;
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div className="App">
-      <Carousel photos={carouselPhotos} title={carouselTitle} />
+      <CoinFlipper />
     </div>
   );
 }
---
+++ src/CoinFlipper.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const HEADS_URL =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/2017" +
+  "-D_Roosevelt_dime_obverse_transparent.png/912px-2017-D_Roosev" +
+  "elt_dime_obverse_transparent.png";
+const TAILS_URL =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/2017" +
+  "-D_Roosevelt_dime_reverse_transparent.png/911px-2017-D_Roosev" +
+  "elt_dime_reverse_transparent.png";
+
+function CoinFlipper() {
+  //have some state for our coin
+  //const [isHeads, setIsHeads ] = useState();
+  //have some state for how many times the coin is flipped
+  const [flips, setFlips] = useState([]);
+
+  function handleClick() {
+    const nextFlip = Math.random() > 0.5 ? "heads" : "tails";
+    setFlips(() => {
+      return [...flips, nextFlip];
+    });
+  }
+
+  const imgSource = flips[flips.length - 1] === "heads" ? HEADS_URL : TAILS_URL;
+  const headsCount = flips.filter((f) => f === "heads").length;
+
+  return (
+    <div>
+      <h1>Let's flip a coin!</h1>
+      <img src={imgSource} style={{ width: "200px" }} />
+      <br></br>
+      <button onClick={handleClick}>Flip Me!</button>
+      <p>
+        Out of {flips.length}, there have been {headsCount} heads and{" "}
+        {flips.length - headsCount} tails.
+      </p>
+    </div>
+  );
+}
+
+export default CoinFlipper;
